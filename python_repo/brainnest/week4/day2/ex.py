c8355f6a3ced4db744979614bded45b27e5aec68
luisfelipequesada <luisfelipedeveloper@gmail.com>
2023-02-09 09:42:03 +0100

    added .gitignore

--- python_repo/brainnest/week4/day2/ex.py
+++ python_repo/brainnest/week4/day2/ex.py
@@ -7,10 +7,10 @@ print(flatten_list(list_c))
 #Use a list comprehension to extract specific elements from a list of dictionaries.
 def extract_elements(dict_list, key):
     return [i[key] for i in dict_list]
-#list of dictionaries?
-dicts = [{ "name": "Tom", "age": 10 },
-         { "name": "Mark", "age": 5 },
-         { "name": "Pam", "age": 7 },
-         { "name": "Jane", "age": 12 }
-        ]
+
+dicts = [{ "name": "Tom", "age": 10, "city": "Madrid" },
+         { "name": "Mark", "age": 5, "city": "London" },
+         { "name": "Pam", "age": 7, "city": "Paris" },
+         { "name": "Jane", "age": 12, "city": "Berlin" }]
+print(extract_elements(dicts, "city"))
 
