202724cb5316d40da2defa0ab66b7573ffea4d53
Luis Felipe Quesada Ramirez <luisfelipequesada@gmail.com>
2024-08-21 08:43:23 +0200

    Changed views to class-based views

--- Django/p01/crm/leads/views.py
+++ Django/p01/crm/leads/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
+#-------------- Class-based view --------------
 class LandingPageView(TemplateView):
     template_name = "landing.html"
 
+class LeadListView(ListView):
+    template_name = "leads/lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
+
+class LeadCreateView(CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+class LeadUpdateView(UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+    context_object_name = "lead"
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+class LeadDeleteView(DeleteView):
+    template_name = 'leads/lead_delete.html'
+    queryset = Lead.objects.all()
+    
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+#-------------- Function-based view --------------
 def landing_page(request):
     return render(request, "landing.html")
 
@@ -56,62 +91,3 @@ def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)
     lead.delete()
     return redirect("/leads")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def lead_create_backup(request):
-    form  = LeadForm()
-    if request.method == 'POST':
-        print("Receiving a post request")
-        form = LeadForm(request.POST)
-        if form.is_valid():
-            form.cleaned_data
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-                )
-            return redirect("/leads")
-    context = {
-        "form": form
-    }
-    return render(request, "leads/lead_create.html", context)
-
-def lead_update_backup(request, pk):
-    lead = Lead.objects.get(id=pk)
-    form  = LeadForm()
-    if request.method == 'POST':
-        form = LeadForm(request.POST)
-        if form.is_valid():
-            form.cleaned_data
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            lead.first_name=first_name
-            lead.last_name=last_name
-            lead.age=age
-            lead.agent=agent
-            lead.save()
-            return redirect("/leads")
-    context = {
-        "lead": lead,
-        'form': form
-    }
-    return render(request, "leads/lead_update.html", context)
